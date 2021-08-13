2deed68bc5885f65fe665c431cc6d43a1f59dab6
Fernando-Escritorio <fernando.takashi12@gmail.com>
2021-08-13 16:16:55 -0300

    first commit

---
+++ js/validacao.js
@@ -0,0 +1,34 @@
+export const valida = (input) => {
+  const tipoDeInput = input.dataset.tipo;
+
+  if (validadores[tipoDeInput]) {
+    validadores[tipoDeInput](input);
+  }
+};
+
+const validadores = {
+  dataNascimento: (input) => {
+    validaDataNascimento(input);
+  },
+};
+
+const validaDataNascimento = (input) => {
+  const dataRecebida = new Date(input.value);
+  let mensagem = "";
+
+  if (!maiorQue18(dataRecebida)) {
+    mensagem = "Você deve ser maior que 18 anos para se cadastrar";
+  }
+  input.setCustomValidity(mensagem);
+};
+
+const maiorQue18 = (data) => {
+  const dataAtual = new Date();
+  const dataMais18 = new Date(
+    data.getUTCFullYear() + 18,
+    data.getUTCMonth(),
+    data.getUTCDate()
+  );
+
+  return dataMais18 <= dataAtual;
+};
